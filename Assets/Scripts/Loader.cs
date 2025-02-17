ac9c8fcf8e462c86144def23c7574530bab41b4d
recursehex <85970576+recursehex@users.noreply.github.com>
2025-02-16 19:36:54 -0800

    remove unused scripts

--- Assets/Scripts/Loader.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Loader : MonoBehaviour
-{
-    public GameObject gameManager;
-    // Start is called before the first frame update
-    void Awake() { if (GameManager.Instance == null) Instantiate(gameManager); }
-}
