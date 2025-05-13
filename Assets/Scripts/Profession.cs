3260b1e50b6c770b842c6e299ed02f5076c805c8
recursehex <85970576+recursehex@users.noreply.github.com>
2025-05-13 11:56:08 -0700

    revamped Profession

--- Assets/Scripts/Profession.cs
+++ Assets/Scripts/Profession.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 public struct Profession
 {
 	public enum Tags
@@ -10,11 +13,19 @@ public struct Profession
 		Ranger,
 		Unknown
 	}
-	public Tags Tag { get; set; }
+	public Tags Tag;
 	public int Level { get; set; }
 	public Profession(Tags Tag, int Level)
 	{
 		this.Tag = Tag;
 		this.Level = Level;
 	}
+	public static readonly Profession Medic 	= new(Tags.Medic, 		0);
+    public static readonly Profession Mechanic 	= new(Tags.Mechanic, 	0);
+    public static readonly Profession Hunter 	= new(Tags.Hunter, 		0);
+    public static readonly Profession Hiker 	= new(Tags.Hiker, 		0);
+    public static readonly Profession Navigator = new(Tags.Navigator, 	0);
+	public static readonly Profession Ranger 	= new(Tags.Ranger, 		0);
+	public static readonly List<Profession> ProfessionList = new() { Medic, Mechanic, Hunter, Hiker, Navigator, Ranger };
+	public static Profession GetRandomProfession() => ProfessionList[Random.Range(0, ProfessionList.Count)];
 }
