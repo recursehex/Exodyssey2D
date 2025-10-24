011987b0b7635837ce310579f0de0453fd10b8b9
recursehex <85970576+recursehex@users.noreply.github.com>
2025-10-23 18:50:33 -0700

    cache Item sprites

--- Assets/Scripts/Entities/Item.cs
+++ Assets/Scripts/Entities/Item.cs
@@ -1,8 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
 	public ItemInfo Info;
-	// Item sprite name must be lowercase of Tag
-	public Sprite GetSprite() => Resources.Load<Sprite>($"Sprites/{Info.Tag.ToString().ToLower()}");
+	private static readonly Dictionary<ItemInfo.Tags, Sprite> SpriteCache = new();
+	public Sprite GetSprite()
+	{
+		if (Info == null)
+		{
+			return null;
+		}
+		if (SpriteCache.TryGetValue(Info.Tag, out Sprite cachedSprite))
+		{
+			return cachedSprite;
+		}
+		string resourcePath = $"Sprites/{Info.Tag.ToString().ToLowerInvariant()}";
+		Sprite sprite = Resources.Load<Sprite>(resourcePath);
+		if (sprite != null)
+		{
+			SpriteCache[Info.Tag] = sprite;
+		}
+		else
+		{
+			Debug.LogWarning($"Sprite not found at path {resourcePath} for Item Tag {Info.Tag}.");
+		}
+		return sprite;
+	}
 }
